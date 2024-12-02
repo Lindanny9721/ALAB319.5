937b99d525bea382f9d6cc290e2927d08ee4b701
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-12-02 13:47:20 -0500

    Changed route for delete id

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -56,9 +56,7 @@ router.patch("/:id/remove", async (req, res) => {
 
 // Delete a single grade entry
 router.delete("/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
-  let result = await collection.deleteOne(query);
+  let result = await Grade.deleteOne({ _id: req.params.id });
 
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
