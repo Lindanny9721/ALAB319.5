4f50fdf6601f5c162744b41e65ee2eb92ffa1999
Lindanny9721 <32419597+Lindanny9721@users.noreply.github.com>
2024-12-02 13:53:31 -0500

    Changed class/id path

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -92,12 +92,11 @@ router.delete("/learner/:id", async (req, res) => {
 
 // Get a class's grade data
 router.get("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
   let query = { class_id: Number(req.params.id) };
 
   // Check for learner_id parameter
   if (req.query.learner) query.learner_id = Number(req.query.learner);
-  let result = await collection.find(query).toArray();
+  let result = await Grade.find(query);
 
   if (!result) res.send("Not found").status(404);
   else res.send(result).status(200);
